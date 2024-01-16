3e2e1f66451abc6d5bc535595f28b56234bfd216
cloggedtoaster <jjavier848@outlook.com>
2024-01-15 23:14:16 -0600

    call serverside to clientside

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
-
+import React from "react"
 import './App.css';
 import Routing from "./routes/index";
 
-
 function App() {
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("http://localhost:5000/")
+        .then(res => res.json())
+        .then(resData => {
+          setData(resData);
+        })
+        .catch(err => console.log(err));
+  }, []); 
+
   return (
     <div className="App">
       <header className="App-header">
-        Content here
+        {!data ? "Not Loaded" : data.message}
       </header>
       <Routing />
     </div>
