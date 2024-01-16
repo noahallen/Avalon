2d657ac3d7a571af395f0cc519e1d64e9df6dacb
Peter Jiang <pzjiang01@hotmail.com>
2024-01-15 23:16:48 -0500

    initial folders

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <h3>
+                HomePage
+            </h3>
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+const NotFound = () => {
+
+    return(
+        <div>
+            Nothing at this URL!
+        </div>
+    );
+}
+
+export default NotFound;
