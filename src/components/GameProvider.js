f1e13c5200077e742f2259ce806127ea1e0e9984
noahallen <noahcallen@gmail.com>
2024-01-20 16:29:01 -0600

    Added workspace tasks/extensions and also a game state context

--- src/components/GameProvider.js
+++ src/components/GameProvider.js
@@ -1,18 +1,17 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 export const GameContext = React.createContext();
 
 export const GameProvider = ({ children }) => {
   const [gameState, setGameState] = React.useState({
-    //The numPlayers is how many players are in a game
+    // numPlayers: represents the number of players in a game.
     numPlayers: 0,
 
-    //Here the players object should contain:
-    // 1. The player's displayName
-    // 2. The player's userName
-    // 3. The player's index relative to the admin (with admin being 0)
-    // 4. The player's role in the game
+    // The players object should contain the following properties for each player:
+    // 1. displayName: The player's display name.
+    // 2. userName: The player's user name.
+    // 3. index: The player's index relative to the admin (with admin being 0).
+    // 4. role: The player's role in the game.
     players: {},
   });
 
