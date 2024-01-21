b6f399fbd583107f900cebd9fe97c628d96ab693
Peter Jiang <pzjiang01@hotmail.com>
2024-01-21 17:07:15 -0500

    game setup

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,23 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useSyncExternalStore } from 'react';
+import apiFunctions from '../../firebase/api';
+
 import { GameContext } from "../components/GameProvider.js";
 import JoinBox from "../components/box-join";
 
 const HomePage = () => {
-	return (
-		<div>
-			<h3>HomePage</h3>
-		</div>
-	);
-};
+    const [players, setPlayers] = useState({});
+    const [goodRoles,setGoodRoles] = useState([]);
+    const [badRoles, setBadRoles] = useState([]);
+
+
+
+    return (
+        <div>
+            <h3>
+                HomePage
+            </h3>
+        </div>
+    );
+}
 
 export default HomePage;
