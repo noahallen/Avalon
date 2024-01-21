c78b066ffe69ccd30e5b2831a102192dfaa6a855
noahallen <noahcallen@gmail.com>
2024-01-21 16:17:29 -0600

    Fixed unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
+import React from "react";
 import App from "./App";
-import { GameContext } from "./components/GameProvider";
+import { GameContextProvider } from "./components/GameProvider";
 
 /*
 test('renders learn react link', () => {
@@ -9,14 +10,20 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 */
+
 // Test suite for App component
 describe("App component", () => {
 	// Test case: App renders without crashing
 	test("renders without crashing", () => {
 		// Render the App component
-		const { container } = render(<App />);
+		const { container } = render(
+			<React.StrictMode>
+				<GameContextProvider>
+					<App />
+				</GameContextProvider>
+			</React.StrictMode>,
+		);
 
-		// Check if the App component rendered
 		expect(container).toBeTruthy();
 	});
 });
