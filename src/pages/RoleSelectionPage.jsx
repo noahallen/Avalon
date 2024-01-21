47c8be00f18a3a64bb6dc04d05ba64069a8c33a0
cloggedtoaster <jjavier848@outlook.com>
2024-01-21 13:08:16 -0600

    Created template pages for planned screens and added routing

---
+++ src/pages/RoleSelectionPage.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const RoleSelectionPage = () => {
+    return (
+        <div>
+            <h2 className='page-header'>
+                Role Selection
+            </h2>
+        </div>
+    );
+}
+
+export default RoleSelectionPage;
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,14 @@ import {
 	Route,
 	Routes,
 } from "react-router-dom";
+
+// Page Imports
 import HomePage from "../pages/HomePage";
 import NotFound from "../pages/NotFound";
+import FeatureSelectionPage from "../pages/FeatureSelectionPage";
+import RoleSelectionPage from "../pages/RoleSelectionPage";
+import RoomCreationPage from "../pages/RoomCreationPage";
+
 import TestState from "../pages/TestStatePage";
 const Routing = (props) => {
 	return (
@@ -15,6 +21,22 @@ const Routing = (props) => {
 				<Route exact path="/" element={<HomePage />} />
 				<Route exact path="/test" element={<TestState />} />
 				<Route exact path="*" element={<NotFound />} />
+
+				<Route
+					exact
+					path="room-creation"
+					element={<RoomCreationPage />}
+				/>
+				<Route
+					exact
+					path="feature-selection"
+					element={<FeatureSelectionPage />}
+				/>
+				<Route
+					exact
+					path="role-selection"
+					element={<RoleSelectionPage />}
+				/>
 			</Routes>
 		</Router>
 	);
