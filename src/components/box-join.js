f37d3e95f91689f1e3e1dc679529bda80ee84592
cloggedtoaster <jjavier848@outlook.com>
2024-01-20 18:41:16 -0600

    Created box-join component and set up button forms for functionality

--- src/components/box-join.js
+++ src/components/box-join.js
@@ -1,5 +1,43 @@
+import { useState } from "react";
 
-const box
+const JoinBox = () =>{
+    const [joinCode, setJoinCode] = useState('')
+    const [error, setError] = useState(null)
+
+    const handleCreate = async(e) => {
+        // prevents reload on submit
+        e.preventDefault();
+
+        console.log("handle create")
+        // handle create button click 
+    }
+    const handleJoin = async(e) => {
+        // prevents reload on submit
+        e.preventDefault();
+        
+        console.log("handle join")
+        // handle join button click
+    }
+
+    return (
+        <div className="box-join">
+            {/* "Create a room" button */}
+            <form onSubmit={handleCreate} >
+                <button>Create a room</button>
+            </form>
+
+            {/* "Join the Resistance" code and button entry */}
+            <form onSubmit={handleJoin} >
+                <h3>Join the Resistance:</h3>
+                <input
+                    type="text"
+                />
+                <button>Join!</button>
+            </form>
+        </div>
+    
+    )
+}
 
 
-export default BoxJoin
+export default JoinBox;
