b0b2eedae41f2d4915483ebda304b53606d51819
cloggedtoaster <jjavier848@outlook.com>
2024-01-20 18:40:40 -0600

    Cleaned up template HTML tags and class names; added box-join component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useContext } from "react";
 import { GameContext } from "../components/GameProvider.js";
+import JoinBox from "../components/box-join";
 
 const HomePage = () => {
 	//Add the GameContext hooks and state you need to the below line
 	const {} = useContext(GameContext);
 	return (
 		<div>
-			<h3>HomePage</h3>
+			<h2>HomePage</h2>
+
+			<JoinBox />
 		</div>
 	);
 };
