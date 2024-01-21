ad3b8be8bef60e1c7f505d904aa6e89bf9ff19d4
noahallen <noahcallen@gmail.com>
2024-01-21 16:08:36 -0600

    Add more state management hooks to GameProvider and added examples of how to use them to new TestStatePage.jsx

--- src/components/GameProvider.js
+++ src/components/GameProvider.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const GameContext = React.createContext();
 
-export const GameProvider = ({ children }) => {
+export const GameContextProvider = ({ children }) => {
 	const [gameState, setGameState] = React.useState({
 		// numPlayers: represents the number of players in a game.
 		numPlayers: 0,
@@ -14,9 +14,112 @@ export const GameProvider = ({ children }) => {
 		// 4. role: The player's role in the game.
 		players: {},
 	});
+	const goodRoles = {
+		Merlin: "Merlin",
+		Percival: "Percival",
+		Servant: "Loyal Servant of Arthur",
+		Troublemaker: "Troublemaker",
+		Cleric: "Cleric",
+		UntrustworthyServant: "Untrustworthy Servant",
+		GoodLancelot: "Good Lancelot",
+		GoodSorcerer: "Good Sorcerer",
+		GoodRogue: "Good Rogue",
+		SeniorMessenger: "Senior Messenger",
+		JuniorMessenger: "Junior Messenger",
+	};
+
+	const evilRoles = {
+		Mordred: "Mordred",
+		Morgana: "Morgana",
+		Oberon: "Oberon",
+		Assassin: "Assassin",
+		Minion: "Minion of Mordred",
+		Trickster: "Trickster",
+		Lunatic: "Lunatic",
+		Brute: "Brute",
+		Revealer: "Revealer",
+		EvilLancelot: "Evil Lancelot",
+		EvilSorcerer: "Evil Sorcerer",
+		EvilRogue: "Evil Rogue",
+		EvilMessenger: "Evil Messenger",
+	};
+	const createPlayer = (userName, displayName) => {
+		if (!userName) {
+			console.error("Invalid userName");
+			return;
+		}
+		if (!displayName) {
+			console.error("Invalid displayName");
+			return;
+		}
+		return {
+			userName: userName,
+			displayName: displayName,
+			index: 0,
+			role: "",
+		};
+	};
+
+	const addPlayer = (player) => {
+		if (player && player.userName && player.displayName) {
+			setGameState((prevState) => ({
+				numPlayers: prevState.numPlayers + 1,
+				players: {
+					...prevState.players,
+					[player.userName]: player,
+				},
+			}));
+		} else {
+			console.error("Invalid player object or properties");
+		}
+	};
+
+	const addPlayerRole = (userName, role) => {
+		if (userName && role && (!!goodRoles[role] || !!evilRoles[role])) {
+			setGameState((prevState) => ({
+				numPlayers: prevState.numPlayers,
+				players: {
+					...prevState.players,
+					[userName]: {
+						...prevState.players[userName],
+						role: role,
+					},
+				},
+			}));
+		} else {
+			console.error("Invalid userName or role");
+		}
+	};
+
+	const addPlayerIndex = (userName, index) => {
+		if (userName && index && index >= 1) {
+			setGameState((prevState) => ({
+				numPlayers: prevState.numPlayers,
+				players: {
+					...prevState.players,
+					[userName]: {
+						...prevState.players[userName],
+						index: index,
+					},
+				},
+			}));
+		} else {
+			console.error("Invalid userName or index");
+		}
+	};
 
 	return (
-		<GameContext.Provider value={{ gameState, setGameState }}>
+		<GameContext.Provider
+			value={{
+				gameState,
+				addPlayer,
+				createPlayer,
+				addPlayerRole,
+				addPlayerIndex,
+				goodRoles,
+				evilRoles,
+			}}
+		>
 			{children}
 		</GameContext.Provider>
 	);
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GameContext } from "../components/GameProvider.js";
 
 const HomePage = () => {
+	//Add the GameContext hooks and state you need to the below line
+	const {} = useContext(GameContext);
 	return (
 		<div>
 			<h3>HomePage</h3>
