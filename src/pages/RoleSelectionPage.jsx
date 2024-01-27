9e812b93f17ecaf2742cd8946ec3515e68134b69
cloggedtoaster <jjavier848@outlook.com>
2024-01-27 10:05:27 -0600

    Added a goodrole to header

--- src/pages/RoleSelectionPage.jsx
+++ src/pages/RoleSelectionPage.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from "react";
+import { GameContext } from "../components/GameProvider.js";
 
 const RoleSelectionPage = () => {
+
+    const {
+		gameState,
+		addPlayerRole,
+        goodRoles,
+        evilRoles,
+	} = useContext(GameContext);
+    const userName1 = "hi";
+    addPlayerRole(userName1, goodRoles.Merlin);
+
     return (
         <div>
             <h2 className='page-header'>
-                Role Selection
+                {goodRoles.Merlin}
             </h2>
         </div>
     );
