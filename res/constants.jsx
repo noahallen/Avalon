8fb98c1dbbcc2e31199208c4d6e51b61981d891c
Peter Jiang <pzjiang01@hotmail.com>
2024-01-27 12:00:36 -0500

    merge

---
+++ res/constants.jsx
@@ -0,0 +1,39 @@
+const GoodRoles = {
+    Merlin: 'Merlin',
+    Servant: 'Loyal Servant',
+    Percival: 'Percival',
+    Cleric: 'Cleric',
+    Trouble: 'TroubleMaker',
+    Lance: 'Good Lancelot',
+    Rogue: 'Good Rogue',
+    Sorcerer: 'Good Sorcerer',
+    Junior: 'Junior Messenger',
+    Senior: ' Senior Messenger',
+    Untrust: 'Untrustworthy Servant',
+}
+
+const BadRoles = {
+    Assassin: 'Assassin',
+    Mordred: 'Mordred',
+    Minion: 'Minion of Mordred',
+    Oberon: 'Oberon',
+    Morg: 'Morgana',
+    Lunatic: 'Lunatic',
+    Brute: 'Brute',
+    Revealer: 'Revealer',
+    Trick: 'Trickster',
+    Lance: 'Evil Lancelot',
+    Rogue: 'Evil Rogue',
+    Sorcerer: 'Evil Sorcerer',
+    Message: 'Evil Messenger',
+
+}
+
+
+
+const Constants = {
+    GoodRoles,
+    BadRoles,
+}
+
+export default Constants;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,15 +4,14 @@ import apiFunctions from '../firebase/api';
 import { GameContext } from "../components/GameProvider.js";
 import JoinBox from "../components/box-join";
 
-const HomePage = () => {
 
-    return (
-        <div>
-            <h3>
-                HomePage 
-            </h3>
-        </div>
-    );
-}
+
+const HomePage = () => {
+	return (
+		<div>
+			<h3>HomePage</h3>
+		</div>
+	);
+};
 
 export default HomePage;
