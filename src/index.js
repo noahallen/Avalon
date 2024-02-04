eee7894def9f80009c5f40f26a39d009c5854361
ayredkklc <derya1.kilic.3@gmail.com>
2024-02-04 15:58:14 -0600

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -5,16 +7,40 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { GameContextProvider } from "./components/GameProvider";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-	<React.StrictMode>
+	<StrictMode>
 		<GameContextProvider>
 			<App />
 		</GameContextProvider>
-	</React.StrictMode>,
+	</StrictMode>,
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+// import { GameContextProvider } from "./components/GameProvider";
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(
+// 	<StrictMode>
+// 		<App />
+// 	</StrictMode>,
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
