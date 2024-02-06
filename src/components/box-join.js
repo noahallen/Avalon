789bb84019b47edf962ca80f8a251be0ae34ec0f
Peter Jiang <pzjiang01@hotmail.com>
2024-02-05 21:15:12 -0500

    create intermediate page for QR code to redirect with gameid and iscreate state

--- src/components/box-join.js
+++ src/components/box-join.js
@@ -8,14 +8,14 @@ const JoinBox = (props) => {
 	const handleCreate = async (e) => {
 		e.preventDefault();
 		navigate("/enterName", {
-			state: { isCreate: true, roomId: "PLACEHOLDER" },
+			state: { isCreate: true, gameID: "PLACEHOLDER" },
 		});
 	};
 
 	const handleJoin = async (e) => {
 		e.preventDefault();
 		navigate("/enterName", {
-			state: { isCreate: false, roomId: inputValue },
+			state: { isCreate: false, gameID: inputValue },
 		});
 	};
 
--- src/pages/EnterName.jsx
+++ src/pages/EnterName.jsx
@@ -4,11 +4,12 @@ import { useLocation } from "react-router-dom";
 
 const EnterName = () => {
 	const location = useLocation();
+	console.log(location);
 	return (
 		<div>
 			<UserNameInputBox
 				shouldUseAuth={false}
-				roomId={location.state.roomId}
+				roomId={location.state.gameID}
 				isCreate={location.state.isCreate}
 			/>
 		</div>
